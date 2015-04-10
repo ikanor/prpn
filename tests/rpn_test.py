26e3a0952d03c7a7322f10b7756fe7f563bb7655
ikanor <franky@tyba.com>
2015-04-10 15:46:11 +0200

    Add mini test

---
+++ tests/rpn_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import rpn
+
+
+class TestCase(unittest.TestCase):
+    def test_parsers(self):
+        result = rpn.parsers.math.eval("5 3 *")
+        self.assertEquals(15.0, result)
+
+        result = rpn.parsers.math.parse("5 3 *")
+        self.assertEquals({'*': ('5', '3')}, result)
+
+        result = rpn.parsers.math.export("5 3 *")
+        self.assertEquals("(5 * 3)", result)
